99a1e72289137eca3ff3ca90d5db08eaea8fab40
AymanAD <aymandandan24680@gmail.com>
2025-09-06 13:31:40 +0300

    health route

---
+++ api/routes/index.js
@@ -0,0 +1,10 @@
+
+const router = require("express").Router();
+const apiResponse = require("../utils/apiResponse");
+
+// health route
+router.get("/health", (req, res) => {
+    apiResponse.success(res, { message: "OK" });
+});
+
+module.exports = router;
